c1ebe8826ff2c4962e80ca3453e23e98928938b9
Eko-Prasetyo15 <prasetyoeko822@gmail.com>
2020-02-26 01:27:34 +0700

    second commit

--- bread.js
+++ bread.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+let fs = require('fs');
+const readData = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+const writeData = (data) => fs.writeFileSync('data.json', JSON.stringify(data, null, 3), 'utf8');
+var bodyParser = require('body-parser')
 
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+// parse application/json
+app.use(bodyParser.json())
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-    res.render('index')
+    res.render('index', {
+        json: readData
+    })
+})
+
+app.get('/add', function (req, res) {
+    res.render('add')
+})
+app.get('/add/edit', function (req, res) {
+    res.render('edit')
 })
 
 app.listen(3000, () => {
