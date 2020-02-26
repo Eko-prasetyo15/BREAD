8332083e4c807b309a9ec7023197f9f0c7c5ebdb
Eko-Prasetyo15 <prasetyoeko822@gmail.com>
2020-02-27 00:10:07 +0700

    chalange19

--- bread.js
+++ bread.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 let fs = require('fs');
 const readData = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-const writeData = (data) => fs.writeFileSync('data.json', JSON.stringify(data, null, 3), 'utf8');
+const writeData = (readData) => fs.writeFileSync('./data.json', JSON.stringify(readData, null, 3), 'utf8');
 var bodyParser = require('body-parser')
 
 
@@ -25,10 +25,44 @@ app.get('/', function (req, res) {
 app.get('/add', function (req, res) {
     res.render('add')
 })
-app.get('/add/edit', function (req, res) {
-    res.render('edit')
-})
 
+app.post('/add', (req, res) => {
+    readData.push({
+        string: req.body.string,
+        integer: req.body.integer,
+        float: req.body.float,
+        date: req.body.date,
+        boolean: req.body.boolean
+    })
+    writeData(readData);
+    res.redirect('/');
+});
+
+app.get('/delete/:id', (req, res) => {
+    let id = req.params.id
+    readData.splice(id, 1);
+    writeData(readData);
+    res.redirect('/');
+
+})
+app.get('/edit/:id', (req, res) => {
+    let id = req.params.id;
+    res.render('edit', {
+        item: {...readData[id]},id});
+});
+app.post('/edit/:id', (req, res) => {
+    let id = req.params.id;
+    const newValue = {
+        string: req.body.string,
+        integer: req.body.integer,
+        float: req.body.float,
+        date: req.body.date,
+        boolean: req.body.boolean
+    };
+    readData.splice(id, 1, newValue);
+    writeData(readData);
+    res.redirect('/');
+});
 app.listen(3000, () => {
     console.log("web ini berjalan di localhost:3000")
 })
